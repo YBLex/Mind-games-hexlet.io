d1641fc1ba63946d07cfdee5b333b9da8b52035b
YB <alex56599@yandex.ru>
2019-07-28 18:09:41 +0300

    Correct codestyle according to eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/extensions
-import { sayHello } from "../index.js";
+// eslint-disable-next-line import/named
+import sayHello from '../index';
 
 console.log('Welcome to the Brain Games!');
 sayHello();
