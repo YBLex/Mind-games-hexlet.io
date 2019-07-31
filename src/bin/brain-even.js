b2debeb5f536fc38278d96f9b462b45d53d68740
YB <alex56599@yandex.ru>
2019-07-31 22:35:37 +0300

    Brain-even game

---
+++ src/bin/brain-even.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// eslint-disable-next-line import/named
+import { startGame, askQuestion } from '../index';
+
+startGame(askQuestion);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
 import readlineSync from 'readline-sync';
 
-const sayHello = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+export const welcome = () => console.log('Welcome to the Brain Games!');
+export const evenStart = () => console.log('Answer "yes" if number is even otherwise answer "no".');
+
+const isEven = (num) => {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
 };
 
-export default sayHello;
+let userName;
+const makeUserName = () => {
+  userName = readlineSync.question('May I have your name? ');
+};
+
+export const sayHello = (name) => {
+  console.log(`Hello, ${name}!`);
+};
+
+const generateNumber = () => {
+  const min = 1;
+  const max = 50;
+  const rand = Math.round(min - 0.5 + Math.random() * (max - min + 1));
+  return rand;
+};
+
+export const askQuestion = () => {
+  const number = generateNumber();
+  console.log(`Question: ${number} `);
+  const userAnswer = readlineSync.question('Your answer: ');
+  if ((isEven(number) && userAnswer === 'yes')
+    || (!isEven(number) && userAnswer === 'no')) {
+    return console.log('Correct!');
+  }
+  const anotherAnswer = userAnswer === 'yes' ? 'no' : 'yes';
+  if (userAnswer === 'yes' || userAnswer === 'no') {
+    console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${anotherAnswer}'.`);
+    return askQuestion();
+  }
+  console.log(`${userAnswer} is not and answer. Try again!`);
+  return askQuestion();
+};
+
+export const startGame = () => {
+  welcome();
+  evenStart();
+  makeUserName();
+  sayHello(userName);
+  for (let i = 0; i < 3; i += 1) {
+    askQuestion();
+  }
+  return console.log(`Congratulations, ${userName}!`);
+};
