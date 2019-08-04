ad33ca2dc4452f905137eb1ee7f57042f62462c4
YB <alex56599@yandex.ru>
2019-08-05 00:21:44 +0300

    Add progression game!!!

---
+++ src/bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// eslint-disable-next-line import/named
+import run from '../games/progression';
+
+run();
---
+++ src/games/progression.js
@@ -0,0 +1,32 @@
+import run from '../index';
+import random from '../utilities';
+
+const rules = 'What number is missing in the progression?';
+const progressionLength = random(5, 10);
+const generateProgression = () => {
+  const result = [];
+  let firstElem = random(1, 20);
+  const step = random(1, 7);
+  for (let i = 0; i < progressionLength; i += 1) {
+    result.push(firstElem);
+    firstElem += step;
+  }
+  return result;
+};
+
+const gameData = () => {
+  const arr = generateProgression();
+  const index = random(0, progressionLength);
+  const answer = String(arr[index]);
+  const question = (() => {
+    arr[index] = '..';
+    return arr.join(' ');
+  })();
+
+  return {
+    question,
+    answer,
+  };
+};
+
+export default () => run(rules, gameData);
