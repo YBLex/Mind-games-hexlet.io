fc95c6c3e6499c319da8e279928a898847ab92c1
YB <alex56599@yandex.ru>
2019-08-04 18:43:54 +0300

    Update brain-calc

---
+++ src/games/calc.js
@@ -0,0 +1,23 @@
+import run from '../index';
+import random from '../utilities';
+
+const rules = 'What is the result of the expression?';
+
+const gameData = () => {
+  const num1 = random(1, 10);
+  const num2 = random(1, 10);
+  const elements = [
+    (x, y) => ({ answer: String(x - y), question: `${x} - ${y}` }),
+    (x, y) => ({ answer: String(x + y), question: `${x} + ${y}` }),
+    (x, y) => ({ answer: String(x * y), question: `${x} * ${y}` }),
+  ];
+
+  const data = elements[random(0, elements.length - 1)](num1, num2);
+  const { question, answer } = data;
+  return {
+    question,
+    answer,
+  };
+};
+
+export default () => run(rules, gameData);
