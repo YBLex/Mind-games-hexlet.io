36fcdb4830c25daebdaa202f613881c80e239935
YB <alex56599@yandex.ru>
2019-08-05 21:38:25 +0300

    Fix findDivisor realisation

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -5,7 +5,7 @@ const rules = 'Find the greatest common divisor of given numbers.';
 
 const findDivisor = (a, b) => {
   if (!b) {
-    return String(a);
+    return a;
   }
 
   return findDivisor(b, a % b);
@@ -15,7 +15,7 @@ const gameData = () => {
   const num1 = random(1, 50);
   const num2 = random(1, 50);
   const question = `${num1} ${num2}`;
-  const answer = findDivisor(num1, num2);
+  const answer = String(findDivisor(num1, num2));
   return {
     question,
     answer,
