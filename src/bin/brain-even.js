d1fad1412c90a17681ba4e2711e3a84de570604a
YB <alex56599@yandex.ru>
2019-08-05 22:40:01 +0300

    Delete eslint disables

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/named
 import run from '../games/even';
 
 run();
--- src/bin/brain-prime.js
+++ src/bin/brain-prime.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// eslint-disable-next-line import/named
 import runGame from '../games/prime';
 
 runGame();
