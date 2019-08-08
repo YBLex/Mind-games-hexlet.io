0e0c17bbc73d428534c72d2964aba9079d086ab0
YB <alex56599@yandex.ru>
2019-08-08 10:17:16 +0300

    Fix variable name

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import readlineSync from 'readline-sync';
 
 const roundsNumber = 3;
 
-const runGame = (rules, gameData) => {
+const runGame = (description, gameData) => {
   console.log('Welcome to the Brain Games!\n');
-  console.log(rules);
+  console.log(description);
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
   for (let i = 0; i < roundsNumber; i += 1) {
