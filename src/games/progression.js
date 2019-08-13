38831e43e5e44b105edccf583e6ea2db8678c08f
YB <alex56599@yandex.ru>
2019-08-14 00:21:06 +0300

    Realised generateProgression as a recursion function

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,20 +4,19 @@ import random from '../utilities';
 const description = 'What number is missing in the progression?';
 
 const generateProgression = (firstElement, step, length) => {
-  const result = [];
-  for (let i = 0; i < length; i += 1) {
-    const currentElement = firstElement + step * i;
-    result.push(currentElement);
+  if (length - 1 === 0) {
+    return firstElement;
   }
-  return result;
+  const nextElement = firstElement + step;
+  return `${String(firstElement)} ${generateProgression(nextElement, step, length - 1)}`;
 };
 
 const progressionLength = 10;
 const gameData = () => {
-  const firstProgressionElement = random(1, 20);
+  const progressionFirstElement = random(1, 20);
   const progressionStep = random(1, 7);
-  const progressoin = generateProgression(firstProgressionElement, progressionStep,
-    progressionLength);
+  const progressoin = generateProgression(progressionFirstElement, progressionStep,
+    progressionLength).split(' ');
   const hiddenElementIndex = random(0, progressionLength);
   const answer = String(progressoin[hiddenElementIndex]);
   progressoin[hiddenElementIndex] = '..';
